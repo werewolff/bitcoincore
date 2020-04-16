7ecf4185518f92ca42a96b106b4d36449650802f
werewolff <werewolff_@bk.ru>
2020-04-16 23:19:53 +0300

    add uninstall function

--- uninstall.php
+++ uninstall.php
@@ -1,4 +1,25 @@
 <?php
-if( ! defined('WP_UNINSTALL_PLUGIN') ) exit;
+// if uninstall.php is not called by WordPress, die
+if (!defined('WP_UNINSTALL_PLUGIN')) {
+    die;
+}
 
-// проверка пройдена успешно. Удаляем таблицы и остальные данные
+// delete all pages blockchains
+$bitcoincore_posts = get_posts(array('post_type' => 'bitcoincore', 'numberposts' => -1));
+foreach ($bitcoincore_posts as $post) {
+    wp_delete_post($post->ID, true);
+}
+// drop a tables of plugin
+global $wpdb;
+$prefix = $wpdb->prefix;
+
+$tables = array(
+    'btccore_methods_versions',
+    'btccore_versions',
+    'btccore_methods',
+    'btccore_categories',
+    'btccore_blockchains'
+);
+foreach ($tables as $table) {
+    $wpdb->query("DROP TABLE IF EXISTS {$prefix}{$table}");
+}
